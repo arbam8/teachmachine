744d5ba1a05e9290f03d4241aa45e8334b3a3c05
arbam8 <60552303+arbam8@users.noreply.github.com>
2021-03-13 20:35:54 -0600

    Update ImageClassifier.js

--- ImageClassifier.js
+++ ImageClassifier.js
@@ -1,3 +1,7 @@
+<div>Teachable Machine Image Model - p5.js and ml5.js</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/p5.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/addons/p5.dom.min.js"></script>
+<script src="https://unpkg.com/ml5@latest/dist/ml5.min.js"></script>
 <script type="text/javascript"> 
 // Classifier Variable
  let classifier;
